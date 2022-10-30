5f0c030d8a2a013a50a6c785cf682a7c5f29bf8b
Ali Reza <webdeve1083@gmail.com>
2022-10-31 07:35:50 +0800

    Deleted all the files that were related to ReactJS

--- src/components/button/button.jsx
+++
@@ -1,8 +0,0 @@
-import "./button.scss";
-import { Link } from "react-router-dom";
-
-const button = () => {
-  return <Link className="button">button</Link>;
-};
-
-export default button;
--- src/components/navbar/navbar.jsx
+++
@@ -1,7 +0,0 @@
-import "./navbar.scss";
-
-const navbar = () => {
-  return <div>navbar</div>;
-};
-
-export default navbar;
