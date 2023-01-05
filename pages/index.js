ee1f4a16faaac11713bef6ac95569dd7cc8cc97f
Ali Reza <webdeve1083@gmail.com>
2023-01-05 10:24:31 +0800

    Changed the name of header, information1, information to be capatilized..

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import Head from "next/head";
 
-import Header from "../components/home/header";
-import Information from "../components/home/information1";
-import Information2 from "../components/home/information2";
+import Header from "../components/Home/Header";
+import Information from "../components/Home/Information1";
+import Information2 from "../components/Home/Information2";
 
 export default function Home() {
   return (
