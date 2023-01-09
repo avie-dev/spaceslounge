8efa0ccc26fca4c288963d37f42ad2ea23f90fd4
Jaynarayan-vaishnav <Jaynarayan540@gmail.com>
2023-01-09 10:45:22 +0530

    Added the Sign-up page

--- pages/sign-up.js
+++ pages/sign-up.js
@@ -1,11 +1,28 @@
+import Link from "next/link";
 import { motion as m } from "framer-motion";
+import { AiOutlineTwitter } from "react-icons/ai";
 
-const singUp = () => {
+const signUp = () => {
   return (
-    <m.div className="signup" exit={{ opacity: 0 }}>
-      <h1>Sign up</h1>
+    <m.div
+      className="signup h-screen flex justify-center items-center"
+      exit={{ opacity: 0 }}>
+      <section className="flex-col flex gap-10">
+        <button
+          type="button"
+          className="rounded-md px-4 py-3 flex gap-2 bg-[#1DA1F2]">
+          <AiOutlineTwitter className="h-6 w-6" />
+          <div>Sign-in as Host</div>
+        </button>
+        <button
+          type="button"
+          className="rounded-md px-4 py-3 flex gap-2 bg-[#1DA1F2]">
+          <AiOutlineTwitter className="h-6 w-6" />
+          <div>Sign-in as Speaker</div>
+        </button>
+      </section>
     </m.div>
   );
 };
 
-export default singUp;
+export default signUp;
