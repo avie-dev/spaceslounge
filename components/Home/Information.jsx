e04a4b8e47db795d03274aa0305c2c74f8cf0926
Ali Reza <webdeve1083@gmail.com>
2023-01-15 08:47:49 +0800

    Added Information.jsx component

---
+++ components/Home/Information.jsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+export default function Information({ data }) {
+  return (
+    <div
+      className={`flex flex-wrap items-start gap-8 ${
+        data.isImgSecond == false && "flex-row-reverse"
+      }`}>
+      <div className="flex flex-col lg:basis-[50%] gap-2">
+        <h2 className="text-xs font-medium lg:font-bold md:text-base">
+          {data.title1}
+        </h2>
+        <h2 className="text-3xl lg:text-4xl text_gradient font-bold">
+          {data.title2}
+        </h2>
+        <p className="text-base md:text-xl">{data.description}</p>
+      </div>
+      <Image
+        src={data.img}
+        className="mx-auto"
+        alt={`Why-spaces-lounge IMG${data.id}`}
+      />
+    </div>
+  );
+}
